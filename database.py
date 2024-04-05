9b1bec57446caed2bfe6a19643d5df1c542e4ec4
Laziz Kalimbetov <155329834+KalimbetovLM@users.noreply.github.com>
2024-04-05 15:11:31 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine("postgresql://postgres:postgres@localhost:5433/fastapi_project_db",echo=True)
+engine = create_engine("postgresql://postgres:your_password@localhost:5433/fastapi_project_db",echo=True)
 
 Base = declarative_base()
 
